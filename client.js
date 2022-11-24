e448a69a06d1c2c92812886b66082813df6100cd
Carol Cheung <carol.cheung@telus.com>
2022-11-23 16:51:18 -0800

    add client.js and play.js with move commands

--- client.js
+++ client.js
@@ -1,22 +1,19 @@
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
+const connect = function() {
+  console.log("Connecting ...");
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541,
   });
 
-  // interpret incoming data as text
   conn.setEncoding("utf8");
+
   conn.on('connect', () => {
-    console.log('Successfully connected to game server');
+    console.log('Successfully connected to game server 🤗');
+    conn.write('Name: CHT');
   });
-
-  process.stdin.on("data", (data) => {
-    conn.write(data);
-  });
-
+  
   conn.on('data', (data) => {
     console.log(data.toString());
     conn.end();
@@ -24,8 +21,8 @@ const connect = function () {
 
   return conn;
 };
+// connect();
 
-console.log("Connecting ...");
-connect();
-
-module.exports = {connect};
+module.exports = {
+  connect,
+};
