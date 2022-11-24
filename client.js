c9da5deb42ecb21cc7687c0677582afa21f804f2
Carol Cheung <carol.cheung@telus.com>
2022-11-23 16:52:15 -0800

    change to client.js

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ const connect = function() {
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
-    console.log('Successfully connected to game server 🤗');
+    console.log('Successfully connected to game server!');
     conn.write('Name: CHT');
   });
   
